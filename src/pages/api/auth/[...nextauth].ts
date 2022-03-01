63652eb504ee3d16aab69866c7adbf95b529026b
robertotics4 <robertooliveiragp@gmail.com>
2022-02-28 22:35:12 -0300

    feat: saving user in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
-import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
 
+import NextAuth from 'next-auth';
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -14,6 +17,40 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const { email: user_email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user_email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 });
 
 
