01c194a794126b1f363d28227e11afcf1a6c98eb
robertotics4 <robertooliveiragp@gmail.com>
2022-03-10 11:20:35 -0300

    feat: stripe subscription

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -17,8 +17,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
-    
-
     const user = await fauna.query<User>(
       q.Get(
         q.Match(
@@ -48,8 +46,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
       customerId = stripeCustomer.id;
     }
-
-    
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: customerId,
